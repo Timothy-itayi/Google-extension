a4c4d0f5f111fa8c96d7e342429dcd1dce768248
Timothy itayi <timmytam10@gmail.com>
2023-08-04 15:12:37 +1200

    successfully format and display weatherData

--- js/index.js
+++ js/index.js
@@ -1,22 +1,39 @@
 // index.js
 
-// Function to fetch weather data from your backend server
+// Function to fetch weather data from the weather API
 async function fetchWeatherData() {
+  const apiUrl =
+    'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m&current_weather=true'
+
   try {
-    const response = await fetch('http://localhost:3000/getWeatherData')
+    const response = await fetch(apiUrl)
     const data = await response.json()
 
     // Process the weather data if needed and update the DOM
     const weatherInfoDiv = document.getElementById('weatherInfo')
     weatherInfoDiv.innerHTML = '<h2>Weather Information</h2>'
 
-    // Example: Display the first hour's time and temperature
-    const time = data.time
-    const temperature = data.temperature
-    weatherInfoDiv.innerHTML += `
-      <p>Time: ${time}</p>
-      <p>Temperature: ${temperature} °C</p>
-    `
+    // Check if the data contains the hourly key and it is an object with time and temperature_2m properties
+    if (
+      data.hasOwnProperty('hourly') &&
+      data.hourly.hasOwnProperty('time') &&
+      data.hourly.hasOwnProperty('temperature_2m')
+    ) {
+      const hourlyTime = data.hourly.time
+      const hourlyTemperature = data.hourly.temperature_2m
+
+      // Display hourly time and temperature
+      for (let i = 0; i < hourlyTime.length; i++) {
+        const time = new Date(hourlyTime[i]).toLocaleTimeString()
+        const temperature = hourlyTemperature[i]
+        weatherInfoDiv.innerHTML += `
+          <p>Time: ${time}</p>
+          <p>Temperature: ${temperature} °C</p>
+        `
+      }
+    } else {
+      console.error('Invalid weather data format:', data)
+    }
   } catch (error) {
     console.error('Error fetching weather data:', error)
   }
